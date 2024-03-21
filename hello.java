340a2339202ebe070b688a7ed1d88e9152c878bf
prajaktag-20 <106527966+prajaktag-20@users.noreply.github.com>
2024-03-21 21:40:59 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World from GIT. Triggered by webhook");
+        System.out.println("Hello World from GIT. Triggered by webhook.");
     }
 }
